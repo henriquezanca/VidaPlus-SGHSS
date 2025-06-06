500e5bee7640a79ee5dfa54a9237703a833c2d22
Henrique Zanca <henrique-zanca@hotmail.com>
2025-06-05 23:03:45 -0300

    configurar middlewares nas rotas

--- start/routes.ts
+++ start/routes.ts
@@ -1,10 +1,23 @@
 import router from '@adonisjs/core/services/router'
 import User from '#models/user'
+import { middleware } from '#start/kernel'
 
-router.post('login', async ({ request, auth }) => {
-  const { email, password } = request.all()
-  const user = await User.verifyCredentials(email, password)
+router
+  .group(() => {
+    router.post('login', async ({ request, auth }) => {
+      const { email, password } = request.all()
+      const user = await User.verifyCredentials(email, password)
 
-  // to generate a token
-  return await auth.use('jwt').generate(user)
-})
+      // to generate a token
+      return await auth.use('jwt').generate(user)
+    })
+
+    router
+      .group(() => {
+        router.get('/', async ({ auth }) => {
+          return auth.getUserOrFail()
+        })
+      })
+      .use(middleware.auth())
+  })
+  .prefix('api')
