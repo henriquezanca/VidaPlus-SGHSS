f6b8a027335f1c17b148d1a1ddebd93480fd9254
Henrique Zanca <henrique-zanca@hotmail.com>
2025-06-12 18:08:28 -0300

    atualizar seeder de usuário

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -4,8 +4,8 @@ import User from '#models/user'
 export default class extends BaseSeeder {
   async run() {
     await User.create({
-      fullName: 'Administrator',
-      email: 'admin@vidaplus.com.br',
+      fullName: 'Henrique Zanca',
+      email: 'hzanca@vidaplus.com.br',
       password: 'secret',
     })
   }
